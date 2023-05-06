313c1d3a414339e608f9c1b6858fa04ed9c546bd
LindaOuer <linda.ouerfelli@gmail.com>
2023-05-06 14:37:19 +0100

    DB + Show

--- src/Controller/StudentController.php
+++ src/Controller/StudentController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Student;
+use App\Repository\StudentRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -28,6 +31,19 @@ class StudentController extends AbstractController
             [
                 'list' => $students,
                 'name' => $name,
+            ]
+        );
+    }
+
+    #[Route('/showStudents', name: 'student_showS')]
+    public function showStudents(StudentRepository $st): Response
+    {
+        // $students = $em->getRepository(Student::class)->findAll();
+        $students = $st->findAll();
+        return $this->render(
+            'student/showStudents.html.twig',
+            [
+                'students' => $students
             ]
         );
     }
